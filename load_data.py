b30d9f70cf3057f2cd9186221895bd114b00a0c8
Liwen-Liberty <37435209+Liwen-Liberty@users.noreply.github.com>
2023-08-10 11:25:50 +0800

    edit address

--- load_data.py
+++ load_data.py
@@ -4,14 +4,14 @@ import torch
 from tqdm import tqdm
 #--------------------------加载数据----------------------------
 def load_data(config):
-    score_gene_num_vector=np.load("/home/jby2/SNN-master/code_by_jby/data/score_gene_num_vector.npy")
+    score_gene_num_vector=np.load("./data/score_gene_num_vector.npy")
 
-    # score_mutation=np.load("/home/jby2/SNN-master/code_by_jby/data/score_mutation.npy")
+    # score_mutation=np.load("./data/score_mutation.npy")
 
     train = np.array(score_gene_num_vector[:,1:],dtype=int)
     targets = np.array(score_gene_num_vector[:,0],dtype=float)
-    # data_target =  pd.read_csv(r"/home/jby2/SNN-master/data/Solid_tumor_mutation_status_mat.csv",header=None)
+    # data_target =  pd.read_csv(r"./data/Solid_tumor_mutation_status_mat.csv",header=None)
 
-    # gene_mutation_dia_1=np.load("/home/jby2/SNN-master/code_by_jby/data/gene_mutation_dia_1.npy")
+    # gene_mutation_dia_1=np.load("./data/gene_mutation_dia_1.npy")
     return train,targets
 
